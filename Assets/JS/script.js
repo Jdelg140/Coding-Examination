4eec7fc5cc45bf1f2ec6162b55b2999b12cb9904
jdelg140 <jacobdelgado140@gmail.com>
2023-04-23 00:43:53 -0400

    figured out how to replace the innertext of functions for the questions and answers from the questions array

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,15 +1,22 @@
 //start the quiz
 
-const startButton= document.getElementById("#start-btn")
-const questionElement = document.getElementById("#question")
-const answerBtnElement =document.getElementById("#answer-btns")
-const nextBtnElement =document.getElementById("#next-btn")
-const highScoreElement=document.getElementById("#scores")
+const startButton= document.getElementById("start-btn")
+const questionContainerElement = document.getElementById("question-container")
+const answerBtnElement =document.getElementById("answer-btns")
+const nextBtnElement =document.getElementById("next-btn")
+const hideScoresElement=document.getElementById("score-container")
+const hideIntroElement=document.getElementById("intro")
+const questionElement=document.getElementById("question")
+const firstAnswer=document.getElementById("btn0")
+const secondAnswer=document.getElementById("btn1")
+const thirdAnswer=document.getElementById("btn2")
+const fourthAnswer=document.getElementById("btn3")
 
 
 startButton.addEventListener("click", startGame)
 nextBtnElement.addEventListener("click",nextQuestion)
-highScoreElement.addEventListener("click",highScore)
+
+
 
 
 function highScore(){
@@ -18,8 +25,13 @@ function highScore(){
 
 
 function startGame(){
-question1
 
+startButton.classList.add("hidden")
+questionContainerElement.classList.remove("hidden")
+hideScoresElement.classList.remove("hide-score")
+nextBtnElement.classList.remove("next-btn-hide")
+hideIntroElement.classList.add("hidden")
+showQuestion1()
 }
 
 
@@ -29,47 +41,54 @@ function nextQuestion(){
 }
 
 
-function showQuestion(){
-
+function showQuestion1(){
+    questionElement.innerText=questions[0].question1
+    firstAnswer.innerText=questions[0].text1
+    secondAnswer.innerText=questions[0].text2
+    thirdAnswer.innerText=questions[0].text3
+    fourthAnswer.innerText=questions[0].text4
+    showQuestion2()
 }
 
 //identify questions and answers into objects and arrays
 const questions= [
     {
         question1: "In JavaScript what can arrays contain?",
-        answers1: [
+        
             
-            {text2:"Numbers", wrong:false},
-            {text3:"Strings", wrong:false},
-            {text4:"Booleans", wrong:false},
-            {text1: "All of the above", correct:true}
+            text1:"Numbers",
+            text2:"Strings", 
+            text3:"Booleans", 
+            text4: "All of the above", 
 
 
-        ],
-        question2: "The condition of am if/else statement is stored in?",
-        answers2:[
-            {text5:"Parenthesis",correct: true},
-            {text6:"Curly Brackets",wrong:false},
-            {text7: "Commas",wrong: false},
-            {text8:"Slashes",wrong: false}
-        ],
+    },
+    {  
+    question2: "The condition of am if/else statement is stored in?",
+        
+            text5:"Parenthesis",
+            text6:"Curly Brackets",
+            text7: "Commas",
+            text8:"Slashes",
+    },
+    {
         question3:"Which language is the hardest?",
-        answers3:[
-            {text9:"HTML",wrong:false},
-            {text10:"CSS",wrong: false},
-            {text11:"JavaScript",correct:true},
-            {text12:"English",wrong:false}
-        ],
+      
+            text9:"HTML",
+            text10:"CSS",
+            text11:"JavaScript",
+            text12:"English"
+    },
+    {
         question4:"Which is a helpful tool to debug code?",
-        answers4:[
-            {text13:"Arrays",wrong:false},
-            {text14:"Booleans",wrong:false},
-            {text15:"If/Then Statements",wrong:false},
-            {text16: "Console.log",correct:true}
-        ]
-
-
+      
+            text13:"Arrays",
+            text14:"Booleans",
+            text15:"If/Then Statements",
+            text16: "Console.log"
+        
     }
 
+    
 ]
 
