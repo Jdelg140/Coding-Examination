8fd0d700d53ab192dd28f97ba56e3110d0e93118
jdelg140 <jacobdelgado140@gmail.com>
2023-04-23 01:24:53 -0400

    edited javascript to replace text for questions and answers for each new question

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -14,14 +14,10 @@ const fourthAnswer=document.getElementById("btn3")
 
 
 startButton.addEventListener("click", startGame)
-nextBtnElement.addEventListener("click",nextQuestion)
+nextBtnElement.addEventListener("click",function(){
 
+},true)
 
-
-
-function highScore(){
-
-}
 
 
 function startGame(){
@@ -31,14 +27,14 @@ questionContainerElement.classList.remove("hidden")
 hideScoresElement.classList.remove("hide-score")
 nextBtnElement.classList.remove("next-btn-hide")
 hideIntroElement.classList.add("hidden")
+if(nextBtnElement==true)
 showQuestion1()
 }
 
 
-//click next to move on to the next question
-function nextQuestion(){
 
-}
+//click next to move on to the next question
+
 
 
 function showQuestion1(){
@@ -48,6 +44,31 @@ function showQuestion1(){
     thirdAnswer.innerText=questions[0].text3
     fourthAnswer.innerText=questions[0].text4
     showQuestion2()
+}
+
+function showQuestion2(){
+    questionElement.innerText=questions[1].question2
+    firstAnswer.innerText=questions[1].text5
+    secondAnswer.innerText=questions[1].text6
+    thirdAnswer.innerText=questions[1].text7
+    fourthAnswer.innerText=questions[1].text8
+    showQuestion3()
+}
+function showQuestion3(){
+    questionElement.innerText=questions[2].question3
+    firstAnswer.innerText=questions[2].text9
+    secondAnswer.innerText=questions[2].text10
+    thirdAnswer.innerText=questions[2].text11
+    fourthAnswer.innerText=questions[2].text12
+    showQuestion4()
+}
+function showQuestion4(){
+    questionElement.innerText=questions[3].question4
+    firstAnswer.innerText=questions[3].text13
+    secondAnswer.innerText=questions[3].text14
+    thirdAnswer.innerText=questions[3].text15
+    fourthAnswer.innerText=questions[3].text16
+    
 }
 
 //identify questions and answers into objects and arrays
@@ -64,7 +85,7 @@ const questions= [
 
     },
     {  
-    question2: "The condition of am if/else statement is stored in?",
+    question2: "The condition of if/else statement is stored in?",
         
             text5:"Parenthesis",
             text6:"Curly Brackets",
