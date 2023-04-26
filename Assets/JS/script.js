35c7488368dcac7bff6ca62adbff2b2f6d4881f4
jdelg140 <jacobdelgado140@gmail.com>
2023-04-26 00:46:57 -0400

    fixed countdown to add 10 seconds when the answer is right, and subtract 10 seconds when the answer is wrong. Also optimized score to add and subtract points based on whether the answer is correct/incorrect

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,18 +2,21 @@
 
 const startButton = document.getElementById("start-btn");
 const questionContainerElement = document.getElementById("question-container");
-const answerBtnElement = document.getElementById("answer-btns");
-const nextBtnElement = document.getElementById("next-btn");
 const hideScoresElement = document.getElementById("score-container");
 const hideIntroElement = document.getElementById("intro");
 const questionElement = document.getElementById("question");
-const btnElement = document.getElementById("btn")
+const btnElement = document.getElementById("btn");
+
 let timeElement = document.getElementById("time");
+let scoreElement = document.getElementById("score");
 
 const choiceElementA = document.getElementById("A");
 const choiceElementB = document.getElementById("B");
 const choiceElementC = document.getElementById("C");
 const choiceElementD = document.getElementById("D");
+
+const incorrectElement= document.getElementById("incorrect-prompt");
+const correctElement= document.getElementById("correct-prompt");
 
 
 const startGameElement = document.getElementById("start-btn");
@@ -24,6 +27,7 @@ choiceElementA.addEventListener("click", checkAnswer);
 choiceElementB.addEventListener("click", checkAnswer);
 choiceElementC.addEventListener("click", checkAnswer);
 choiceElementD.addEventListener("click", checkAnswer);
+
 
 
 let counter = 60;
@@ -37,11 +41,11 @@ function countDown() {
 
             if (counter == 0) {
                 clearInterval(interval);
-                timeElement.innerHTML = "Time: " + 0
+                timeElement.innerHTML = "Time: " + 0;
             }
         }, 1000)
     }
-}
+};
 
 
 
@@ -54,48 +58,44 @@ let questions = [
         choiceC: { answer: "Booleans", correct: false },
         choiceD: { answer: "All of the above", correct: true }
     },
+    
+
     {
-        question: "Next Question!?",
-        choiceA: { answer: "blash", correct: false },
-        choiceB: { answer: "blablah", correct: false },
-        choiceC: { answer: "anotherblah", correct: false },
-        choiceD: { answer: "correctBlah", correct: true }
+        question: "The condition of am if/else statement is stored in?",
+
+        choiceA: {answer: "Parenthesis", correct: true},
+        choiceB: {answer: "Curly Brackets", correct: false},
+        choiceC: {answer: "Commas",correct: false},
+        choiceD: {answer: "Slashes",correct: false}
+        
     },
+    {
+        question: "Which language is the hardest?",
 
-    // {
-    //     question: "The condition of am if/else statement is stored in?",
+        choiceA: {answer: "HTML",correct: false},
+        choiceB: {answer: "CSS",correct: false},
+        choiceC: {answer: "JavaScript",correct: true},
+        choiceD: {answer: "English",correct: false}
+      
+    },
+    {
+        question: "Which is a helpful tool to debug code?",
 
-    //     choiceA: "Parenthesis",
-    //     choiceB: "Curly Brackets",
-    //     choiceC: "Commas",
-    //     choiceD: "Slashes",
-    //     correct: "A"
-    // },
-    // {
-    //     question: "Which language is the hardest?",
+        choiceA: {answer: "Arrays",correct: false},
+        choiceB: {answer: "Booleans",correct: false},
+        choiceC: {answer: "If/Then Statements",correct: false},
+        choiceD: {answer: "Console.log",correct: true}
+        
 
-    //     choiceA: "HTML",
-    //     choiceB: "CSS",
-    //     choiceC: "JavaScript",
-    //     choiceD: "English",
-    //     correct: "C"
-    // },
-    // {
-    //     question: "Which is a helpful tool to debug code?",
-
-    //     choiceA: "Arrays",
-    //     choiceB: "Booleans",
-    //     choiceC: "If/Then Statements",
-    //     choiceD: "Console.log",
-    //     correct: "D"
-
-    // }
+    }
 
 
 ];
 
 //click next to move on to the next question
 let questionIndex = 0;
+let score=0;
+
 
 function createQuestion() {
 
@@ -104,17 +104,18 @@ function createQuestion() {
     hideScoresElement.classList.remove("hide-score");
     hideIntroElement.classList.add("hidden");
 
+    
     let q = questions[questionIndex];
-    console.log(q)
-    questionIndex++
+    console.log(q);
+    questionIndex++;
 
     questionElement.innerHTML = q.question;
 
     choiceElementA.innerHTML = q.choiceA.answer;
-    choiceElementA.setAttribute('data-correct', q.choiceA.correct)
+    choiceElementA.setAttribute('data-correct', q.choiceA.correct);
 
     choiceElementB.innerHTML = q.choiceB.answer;
-    choiceElementB.setAttribute('data-correct', q.choiceB.correct)
+    choiceElementB.setAttribute('data-correct', q.choiceB.correct);
 
     choiceElementC.innerHTML = q.choiceC.answer;
     choiceElementC.setAttribute('data-correct', q.choiceC.correct);
@@ -122,24 +123,60 @@ function createQuestion() {
     choiceElementD.innerHTML = q.choiceD.answer;
     choiceElementD.setAttribute('data-correct', q.choiceD.correct);
 
+    scoreElement.innerHTML="Score: "+score;
+   
     countDown();
+
 }
 
+function clearFeedback(){
+    
+    incorrectElement.classList.add("incorrectbtn");
+    correctElement.classList.add("correctbtn");
 
+}
+
+function scoreIncrement(){
+
+    score=score+10;
+}
+
+function scoreDecrement(){
+    
+    score=score-5;
+}
 
 
 function checkAnswer(event) {
     if(event.target.getAttribute('data-correct') === "true"){
-        console.log("Correct answer was chosen, add time, give feedback, call next question func")
-        counter + 10
+        console.log("Correct answer was chosen, add time, give feedback, call next question func");
+        counter=counter+10;
+        scoreElement.innerHTML="Score: "+score;
+        correctElement.classList.remove("correctbtn");
+        scoreIncrement();
         createQuestion();
+       
+        
     }else{
-        console.log("incorrect answer choosen, subtract time, give feedback, call next func")
+        console.log("incorrect answer choosen, subtract time, give feedback, call next func");
+        counter=counter - 10;
+        scoreElement.innerHTML="Score: "+score;
+        incorrectElement.classList.remove("incorrectbtn");
+        scoreDecrement();
+        createQuestion();
     }
 }
 
 
+function endQuiz(){
+    if(questionIndex>questions.length || counter==0){
+        startButton.classList.add("hidden");
+        questionContainerElement.classList.add("hidden");
+        hideScoresElement.classList.add("hide-score");
+        hideIntroElement.classList.add("hidden");
+        incorrectElement.classList.add("incorrectbtn");
+        correctElement.classList.add("correctbtn");
+   
+}
 
-
-
-
+}
